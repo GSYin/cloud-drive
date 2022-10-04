578ad8202134c15283ac95219bcb37b7db6b0643
Ryan Gee <wencungsy@gmail.com>
2022-10-05 00:25:53 +0800

    feat: update user table structure

--- core/models/user.go
+++ core/models/user.go
@@ -1,11 +1,16 @@
 package models
 
+import "time"
+
 type User struct {
-	Id       int    `xorm:"pk autoincr"`
-	Identity string `xorm:"varchar(255) notnull"`
-	Username string `xorm:"varchar(255) notnull"`
-	Password string `xorm:"varchar(255) notnull"`
-	Email    string `xorm:"varchar(255) notnull"`
+	Id        int       `xorm:"pk autoincr"`
+	Identity  string    `xorm:"varchar(255) notnull"`
+	Username  string    `xorm:"varchar(255) notnull"`
+	Password  string    `xorm:"varchar(255) notnull"`
+	Email     string    `xorm:"varchar(255) notnull"`
+	CreatedAt time.Time `xorm:"created"`
+	UpdatedAt time.Time `xorm:"updated"`
+	DeletedAt time.Time `xorm:"deleted"`
 }
 
 func (table User) TableName() string {
