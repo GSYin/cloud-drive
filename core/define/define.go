81baf15de552ac13c06a135f7cc5ec33566f1f45
Ryan Gee <wencungsy@gmail.com>
2022-10-04 15:18:55 +0800

    feat: add general configuration

--- core/define/define.go
+++ core/define/define.go
@@ -1,6 +1,10 @@
 package define
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/spf13/viper"
+	"log"
+)
 
 type UserClaim struct {
 	Id       int
@@ -9,4 +13,99 @@ type UserClaim struct {
 	jwt.StandardClaims
 }
 
-var JwtKey = "ryangee-cloud-drive"
+// CodeLength mail code length
+var CodeLength = 6
+
+// CodeExpireTime mail code expire time second
+var CodeExpireTime = 10 * 60
+
+// CosAddr Tencent COS addr
+var CosAddr = "https://ryan-1254379222.cos.ap-beijing.myqcloud.com"
+
+// PageSize file
+var PageSize = 20
+
+var DateTime = "YYYY-MM-DD HH:m:s"
+
+// TokenExpireTime second
+var TokenExpireTime = 60 * 60
+
+var RefreshTokenExpireTime = 60 * 60 * 24 * 3
+
+func GetJetKey() string {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")    // optionally look for a config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	JetKey, ok := viper.Get("JWT_KEY").(string)
+	if !ok {
+		log.Fatalf("cat not read JWT_KEY")
+	}
+	return JetKey
+}
+
+func GetDatabaseInfo() string {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")    // call multiple times to add many search paths
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	Database, ok := viper.Get("DATABASE").(string)
+	if !ok {
+		log.Fatalf("can not read DATABASE")
+	}
+
+	return Database
+}
+func GetSentEmailInfo() string {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	SentEmailPassword, ok := viper.Get("MAIL_PASSWORD").(string)
+	if !ok {
+		log.Fatalf("can not read MAIL_PASSWORD")
+	}
+
+	return SentEmailPassword
+}
+
+func GetCosSecretId() string {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	CosSecretId, ok := viper.Get("COS_SECRET_ID").(string)
+	if !ok {
+		log.Fatalf("can not read COS_SECRET_ID")
+	}
+
+	return CosSecretId
+}
+
+func GetCosSecretKey() string {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
+	CosSecretKey, ok := viper.Get("COS_SECRET_KEY").(string)
+	if !ok {
+		log.Fatalf("can not read COS_SECRET_KEY")
+	}
+
+	return CosSecretKey
+}
