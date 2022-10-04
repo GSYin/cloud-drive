116cc8726d237350a455d649732e84b9c2352b9e
Ryan Gee <wencungsy@gmail.com>
2022-10-05 00:26:59 +0800

    feat: update user login and detail

--- core/internal/logic/user_login_logic.go
+++ core/internal/logic/user_login_logic.go
@@ -1,8 +1,9 @@
 package logic
 
 import (
+	"cloud-drive/core/define"
 	"cloud-drive/core/models"
-	"cloud-drive/core/util"
+	"cloud-drive/core/utils"
 	"context"
 	"errors"
 
@@ -29,7 +30,7 @@ func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLog
 func (l *UserLoginLogic) UserLogin(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
 	// 1. 从数据库中查询当前用户
 	user := new(models.User)
-	has, err := models.Engine.Where("username = ? AND password = ?", req.Username, util.Md5(req.Password)).Get(user)
+	has, err := l.svcCtx.Engine.Where("username = ? AND password = ?", req.Username, utils.Md5(req.Password)).Get(user)
 	if err != nil {
 		return nil, err
 	}
@@ -37,12 +38,19 @@ func (l *UserLoginLogic) UserLogin(req *types.LoginRequest) (resp *types.LoginRe
 		return nil, errors.New("用户名或密码错误")
 	}
 	// 2. 生成token
-	token, err := util.GenerateToken(user.Id, user.Identity, user.Username)
+	token, err := utils.GenerateToken(user.Id, user.Identity, user.Username, define.TokenExpireTime)
+	if err != nil {
+		return nil, err
+	}
+
+	//3. generate refresh token
+	refreshToken, err := utils.GenerateToken(user.Id, user.Identity, user.Username, define.RefreshTokenExpireTime)
 	if err != nil {
 		return nil, err
 	}
 	resp = new(types.LoginResponse)
 	resp.Token = token
+	resp.RefreshToken = refreshToken
 
 	return
 }
